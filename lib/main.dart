eb2e646db3154f203e5fd426263b3f65d90c5512
VedranSelak <selakvedran@gmail.com>
2021-09-29 22:16:13 +0200

    feat: add UI

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,81 @@ class Card extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: const Text("ID Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
       ),
-      body: const Text('bla'),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 40 ,30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/myPhoto.jpg'),
+                radius: 40,
+              ),
+            ),
+           Divider(
+             height: 60,
+             color: Colors.grey[800],
+           ),
+           const Text(
+             'NAME',
+             style: TextStyle(
+               color: Colors.grey,
+               letterSpacing: 1.5,
+             ),
+           ),
+            const SizedBox(height: 10),
+            Text(
+              'Vedran Selak',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 1.5,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              'CURRENT POSITION',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 1.5,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Flutter developer',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 1.5,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(Icons.mail, color: Colors.grey[400]),
+                Text(
+                  'selakvedran@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    letterSpacing: 1.5,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
